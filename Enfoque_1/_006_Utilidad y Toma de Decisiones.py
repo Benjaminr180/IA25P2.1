145218fb03a145f12a715475e166dd9236177c91
Benjaminr180 <benjaminrori180@gmail.com>
2025-04-02 18:57:13 -0600

    deleted:    Enfoque_1/Planificacion/_001_Busqueda no informada.py 	deleted:    Enfoque_1/Planificacion/_002_Busqueda Informada.py 	deleted:    Enfoque_1/Planificacion/_003_Algoritmos Geneticos.py 	deleted:    "Enfoque_1/Planificacion/_004_Satisfacci\303\263n de Restricciones.py" 	deleted:    Enfoque_1/_005_Juegos.py 	deleted:    Enfoque_1/_006_Utilidad y Toma de Decisiones.py 	deleted:    Enfoque_1/_007_Aprendizaje por Refuerzo.py

--- Enfoque_1/_006_Utilidad y Toma de Decisiones.py
+++
@@ -1,6 +0,0 @@
-def expected_utility(outcomes, probabilities):
-    return sum(o * p for o, p in zip(outcomes, probabilities))
-# Ejemplo de uso
-outcomes = [100, 200, 300]
-probabilities = [0.2, 0.5, 0.3]
-print(expected_utility(outcomes, probabilities)) 
